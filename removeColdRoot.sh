543b370ff2a83dc234c2a92ab7284027fa1deb68
AllWorkAndNoPlay <tcam@ard.local>
2018-05-09 10:21:28 -0700

    Add example script for basic coldroot removal

---
+++ removeColdRoot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script removes example version of coldroot 
+# details on coldroot https://objective-see.com/blog/blog_0x2A.html
+# allworkandnoplay
+
+#unload launchDaemon
+launchctl unload /Library/LaunchDaemons/com.apple.audio.driver.plist
+
+#delete the launchDaemon plist
+rm /Library/LaunchDaemons/com.apple.audio.driver.plist
+
+#delete the binary folder
+rm -Rf /private/var/tmp/com.apple.audio.driver.app
+
+#remove log
+rm /private/var/tmp/adobe_logs.log
